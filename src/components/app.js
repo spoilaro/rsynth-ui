f481cfb061be8b6b69a71efd2f34e4dbb6f4eba7
Johannes Haapanen <johannes.haapanen@gmail.com>
2022-11-04 09:25:22 +0200

    Added processor filers

--- src/components/app.js
+++ src/components/app.js
@@ -9,47 +9,40 @@ let audioCtx = null;
 // let bufferLength;
 // let dataArray;
 let source;
+let input_stream;
 
 const App = () => {
   useEffect(() => {
-    navigator.mediaDevices
-      .getUserMedia({ audio: true })
-      .then(async (stream) => {
-        audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-
-        await audioCtx.audioWorklet.addModule("../processors/testProcessor.js");
-        source = audioCtx.createMediaStreamSource(stream);
-
-        const testProcessor = new AudioWorkletNode(audioCtx, "testProcessor");
-
-        source.connect(testProcessor).connect(audioCtx.destination);
-
-        //
-        // analyser = audioCtx.createAnalyser();
-        // analyser.fftSize = 2048;
-        //
-        // bufferLength = analyser.frequencyBinCount;
-        // dataArray = new Uint8Array(bufferLength);
-        // analyser.getByteTimeDomainData(dataArray);
-        //
-        // source.connect(analyser);
-      });
+    navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+      input_stream = stream;
+    });
   }, []);
 
   const startSound = async () => {
-    // Add processor
-    await audioCtx.audioWorklet.addModule("../processors/testProcessor.js");
-    const oscillator = new OscillatorNode(audioCtx);
-    const bypasser = new AudioWorkletNode(audioCtx, "testProcessor");
-    oscillator.connect(bypasser).connect(audioCtx.destination);
-    oscillator.start();
+    audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+    // await audioCtx.audioWorklet.addModule("../processors/testProcessor.js");
+    await audioCtx.audioWorklet.addModule("../processors/VanillaReverb.js.js");
+    source = audioCtx.createMediaStreamSource(input_stream);
+
+    const testProcessor = new AudioWorkletNode(audioCtx, "vanilla-reverb");
+
+    source.connect(testProcessor).connect(audioCtx.destination);
+  };
+
+  const stopSound = async () => {
+    await audioCtx.stop();
+  };
+
+  const toggleSound = async () => {
+    audioCtx ? await stopSound() : await startSound();
   };
 
   return (
     <div id="app">
       <Navbar />
       <div id="main">
-        <button onClick={startSound}>START</button>
+        <button onClick={toggleSound}>START</button>
       </div>
     </div>
   );
